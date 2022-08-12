25c238df6a7fa9f9aa2686195658c2fabd0b4837
awitten1 <awitten1@terpmail.umd.edu>
2022-08-11 22:02:38 -0400

    add template specialization

--- chapter2/class_templates.cpp
+++ chapter2/class_templates.cpp
@@ -1,12 +1,23 @@
 
 #include <string>
+#include <iostream>
 
 #include "stack.h"
 
 int main() {
     Stack<int> intStack;
+    Stack<int*> intPtrStack;
     Stack<std::string> stringStack;
     intStack.push(1);
+    intStack.push(3);
+    intStack.push(5);
+
+    intPtrStack.push(new int{4});
+    intPtrStack.push(new int{7});
+    intPtrStack.push(new int{8});
+
+    std::cout << intStack << std::endl;
+    std::cout << intPtrStack << std::endl;
 
     return 0;
 }
--- chapter2/stack.h
+++ chapter2/stack.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cassert>
+#include <iostream>
 
 template <typename T>
 class Stack {
@@ -20,5 +21,36 @@ public:
     }
     bool empty() const {
         return elems.empty();
+    }
+    friend std::ostream& operator<< (std::ostream& strm, const Stack<T>& s) {
+        std::copy(s.elems.begin(), s.elems.end(), std::ostream_iterator<T>(std::cout, ", "));
+        return strm;
+    }
+};
+
+
+template <typename T>
+class Stack<T*> {
+private:
+    std::vector<T*> elems;
+
+public:
+    void push(T* elem) {
+        elems.push_back(elem);
+    }
+    void pop() {
+        assert(!empty());
+        elems.pop_back();
+    }
+    const T* top() const {
+        assert(!empty());
+        return elems.back();
+    }
+    bool empty() const {
+        return elems.empty();
+    }
+    friend std::ostream& operator<< (std::ostream& strm, const Stack<T*>& s) {
+        std::copy(s.elems.begin(), s.elems.end(), std::ostream_iterator<T*>(std::cout, "..."));
+        return strm;
     }
 };
